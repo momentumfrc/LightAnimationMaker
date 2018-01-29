62f58bede47aa1401f080c14209d9936bab3715d
Jordan Powers <jordanpowers1227@gmail.com>
2018-01-29 14:02:42 -0800

    Animation List Builder

--- animationBuilder.js
+++ animationBuilder.js
@@ -1,13 +1,14 @@
 var builder = {
   animationBox : function() {
     var baseHTML = `
-    <div class="animationbox"><button class="removeButton">Delete</button></div>
+    <div class="animationbox"><button class="removeButton">Delete</button><div class="animationoptions"></div></div>
     `;
     var elem = $($.parseHTML(baseHTML));
-    elem.append(builder.animationSelector());
+    elem.find(".animationoptions").before(builder.animationSelector());
     elem.find(".removeButton").click(function(e){
       $(e.target).closest(".animationbox").remove();
     });
+    elem.find(".animationoptions").append(builder.solid());
     return elem;
   },
   animationSelector : function() {
@@ -26,8 +27,11 @@ var builder = {
     return elem;
   },
   switchAnimationOptions : function(e) {
-    switch($(e.target).find(".animationType").val()) {
+    var anioptions = $(e.target).closest(".animationbox").find(".animationoptions");
+    anioptions.empty();
+    switch($(e.target).val()) {
       case "group":
+        anioptions.append(builder.group());
         break;
       case "blink":
         break;
@@ -37,10 +41,40 @@ var builder = {
         break;
       case "solid":
         break;
-    },
-    group : function() {
-    },
-    blink : function() {
     }
+  },
+  group : function() {
+    var baseHTML = `
+    <table>
+      <tr><th>Animation</th><th>Time</th></tr>
+      <tr><td rowspan="2"><button class="addsuban">Add</button></td></tr>
+    </table>
+    `;
+    var elem = $($.parseHTML(baseHTML));
+    elem.find(".addsuban").click(function(e) {
+      var baseHTML = '<tr><td class="animationcell"></td><td><input type="number" class="animationtime"></td></tr>';
+      var elem = $($.parseHTML(baseHTML));
+      var anibox = builder.animationBox();
+      anibox.find(".removeButton").off("click");
+      anibox.find(".removeButton").click(function(e){
+        $(e.target).closest("tr").remove();
+      });
+      elem.find(".animationcell").append(anibox);
+      var target = $(e.target);
+      target.closest("tr").before(elem);
+    });
+    return elem;
+  },
+  blink : function() {
+
+  },
+  fade : function() {
+
+  },
+  random : function() {
+
+  },
+  solid : function() {
+
   }
 }
